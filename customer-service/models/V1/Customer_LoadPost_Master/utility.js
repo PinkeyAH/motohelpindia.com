a7c7c4a541229a426e048033c2b1ca3082c967a3
PinkyV <pinkyahirwar752@gmail.com>
2025-12-03 12:40:25 +0530

    multi invoce data add

--- customer-service/models/V1/Customer_LoadPost_Master/utility.js
+++ customer-service/models/V1/Customer_LoadPost_Master/utility.js
@@ -2,7 +2,6 @@ const sql = require('mssql');
 const pool = require('../../../db/db');
 const logger = require('../../../log/logger');
 
-
 exports.customerloadpostmasterDB = (customerloadpostmaster) => {
     logger.info(`[INFO]: Updating customerloadpostmasterDB record for CustomerID: ${customerloadpostmaster.CustomerID}`);
 
@@ -11,66 +10,86 @@ exports.customerloadpostmasterDB = (customerloadpostmaster) => {
             .then(pool => {
                 const request = pool.request();
 
-                // Input parameters
-                // request.input('load_master_id',sql.NVarChar(15), customerloadpostmaster.load_master_id);
-                request.input('LoadPostID', sql.NVarChar(15), customerloadpostmaster.LoadPostID);
-                request.input('CustomerID', sql.NVarChar(15), customerloadpostmaster.CustomerID);
-                request.input('contact_person', sql.NVarChar(100), customerloadpostmaster.contact_person);
-                request.input('contact_number', sql.NVarChar(15), customerloadpostmaster.contact_number);
-                request.input('pickup_plot_unit', sql.NVarChar(100), customerloadpostmaster.pickup_plot_unit);
-                request.input('pickup_area_street', sql.NVarChar(200), customerloadpostmaster.pickup_area_street);
+                // INPUTS (exact order as SP)
+                request.input('LoadPostID', sql.NVarChar(150), customerloadpostmaster.LoadPostID);
+                request.input('CustomerID', sql.NVarChar(150), customerloadpostmaster.CustomerID);
+
+                request.input('contact_person', sql.NVarChar(150), customerloadpostmaster.contact_person);
+                request.input('contact_number', sql.NVarChar(20), customerloadpostmaster.contact_number);
+                request.input('pickup_plot_unit', sql.NVarChar(255), customerloadpostmaster.pickup_plot_unit);
+                request.input('pickup_area_street', sql.NVarChar(255), customerloadpostmaster.pickup_area_street);
                 request.input('pickup_pincode', sql.NVarChar(10), customerloadpostmaster.pickup_pincode);
-                request.input('pickup_state', sql.NVarChar(100), customerloadpostmaster.pickup_state);
-                request.input('pickup_district', sql.NVarChar(100), customerloadpostmaster.pickup_district);
-                request.input('pickup_taluka', sql.NVarChar(100), customerloadpostmaster.pickup_taluka);
-                request.input('pickup_map_location', sql.NVarChar(200), customerloadpostmaster.pickup_map_location);
-                request.input('consignee_name', sql.NVarChar(100), customerloadpostmaster.consignee_name);
-                request.input('consignee_mobile', sql.NVarChar(15), customerloadpostmaster.consignee_mobile);
-                request.input('drop_plot_unit', sql.NVarChar(100), customerloadpostmaster.drop_plot_unit);
-                request.input('drop_area_street', sql.NVarChar(200), customerloadpostmaster.drop_area_street);
+                request.input('pickup_state', sql.NVarChar(50), customerloadpostmaster.pickup_state);
+                request.input('pickup_district', sql.NVarChar(50), customerloadpostmaster.pickup_district);
+                request.input('pickup_taluka', sql.NVarChar(50), customerloadpostmaster.pickup_taluka);
+                request.input('pickup_map_location', sql.NVarChar(255), customerloadpostmaster.pickup_map_location);
+
+                request.input('consignee_name', sql.NVarChar(150), customerloadpostmaster.consignee_name);
+                request.input('consignee_mobile', sql.NVarChar(20), customerloadpostmaster.consignee_mobile);
+                request.input('drop_plot_unit', sql.NVarChar(255), customerloadpostmaster.drop_plot_unit);
+                request.input('drop_area_street', sql.NVarChar(255), customerloadpostmaster.drop_area_street);
                 request.input('drop_pincode', sql.NVarChar(10), customerloadpostmaster.drop_pincode);
-                request.input('drop_state', sql.NVarChar(100), customerloadpostmaster.drop_state);
-                request.input('drop_district', sql.NVarChar(100), customerloadpostmaster.drop_district);
-                request.input('drop_taluka', sql.NVarChar(100), customerloadpostmaster.drop_taluka);
-                request.input('drop_map_location', sql.NVarChar(200), customerloadpostmaster.drop_map_location);
-                request.input('cargo_type', sql.NVarChar(100), customerloadpostmaster.cargo_type);
-                request.input('dhala_length', sql.NVarChar(50), customerloadpostmaster.dhala_length);
+                request.input('drop_state', sql.NVarChar(50), customerloadpostmaster.drop_state);
+                request.input('drop_district', sql.NVarChar(50), customerloadpostmaster.drop_district);
+                request.input('drop_taluka', sql.NVarChar(50), customerloadpostmaster.drop_taluka);
+                request.input('drop_map_location', sql.NVarChar(255), customerloadpostmaster.drop_map_location);
+
+                request.input('cargo_type', sql.NVarChar(20), customerloadpostmaster.cargo_type);
+                request.input('dhala_length', sql.NVarChar(10), customerloadpostmaster.dhala_length);
                 request.input('body_type', sql.NVarChar(100), customerloadpostmaster.body_type);
-                request.input('cargo_content', sql.NVarChar(200), customerloadpostmaster.cargo_content);
+                request.input('cargo_content', sql.NVarChar(100), customerloadpostmaster.cargo_content);
                 request.input('package_type', sql.NVarChar(100), customerloadpostmaster.package_type);
-                request.input('net_weight', sql.Float, customerloadpostmaster.net_weight);
-                request.input('approx_weight', sql.Float, customerloadpostmaster.approx_weight);
-                request.input('gross_weight', sql.Float, customerloadpostmaster.gross_weight);
-                request.input('lab_report_applied', sql.Bit, customerloadpostmaster.lab_report_applied);
-                request.input('lab_report_available', sql.Bit, customerloadpostmaster.lab_report_available);
-                request.input('insurance', sql.Bit, customerloadpostmaster.insurance);
-                request.input('transit_risk', sql.Bit, customerloadpostmaster.transit_risk);
-                request.input('lr_no', sql.NVarChar(50), customerloadpostmaster.lr_no);
-                request.input('lr_date', sql.Date, customerloadpostmaster.lr_date);
-                request.input('po_number', sql.NVarChar(50), customerloadpostmaster.po_number);
-                request.input('invoice_date', sql.NVarChar(50), customerloadpostmaster.invoice_date);
-                // request.input('DATE', sql.Float, customerloadpostmaster.DATE);
-                request.input('invoice_number', sql.NVarChar(50), customerloadpostmaster.invoice_number);
-                request.input('quantity', sql.NVarChar(200), customerloadpostmaster.quantity);
-                request.input('value_amount', sql.NVarChar(200), customerloadpostmaster.value_amount);
-                request.input('remarks', sql.NVarChar(200), customerloadpostmaster.remarks);
 
-                
+                request.input('body_type_remarks', sql.NVarChar(100), customerloadpostmaster.body_type_remarks);
+                request.input('cargo_content_remarks', sql.NVarChar(100), customerloadpostmaster.cargo_content_remarks);
+                request.input('package_type_remarks', sql.NVarChar(100), customerloadpostmaster.package_type_remarks);
 
-                // Outputs
-                request.output('load_master_id', sql.NVarChar(255));
-                request.output('bstatus_code', sql.NVarChar(255));
+                request.input('net_weight', sql.NVarChar(50), customerloadpostmaster.net_weight);
+                request.input('approx_weight', sql.NVarChar(50), customerloadpostmaster.approx_weight);
+                request.input('gross_weight', sql.NVarChar(50), customerloadpostmaster.gross_weight);
+
+                request.input('lab_report_applied', sql.Char(1), customerloadpostmaster.lab_report_applied ? 'Y' : 'N');
+                request.input('lab_report_available', sql.Char(1), customerloadpostmaster.lab_report_available ? 'Y' : 'N');
+                request.input('insurance', sql.Char(1), customerloadpostmaster.insurance ? 'Y' : 'N');
+
+                request.input('transit_risk', sql.NVarChar(255), customerloadpostmaster.transit_risk);
+                request.input('lr_no', sql.NVarChar(255), customerloadpostmaster.lr_no);
+                request.input('lr_date', sql.NVarChar(255), customerloadpostmaster.lr_date);
+
+                // TVP INVOICE DETAILS (must match exact SP TVP definition)
+                const INVD = new sql.Table('invoice_Details');
+                INVD.columns.add('po_number', sql.NVarChar(50));
+                INVD.columns.add('invoice_number', sql.NVarChar(50));
+                INVD.columns.add('invoice_date', sql.NVarChar(50));
+                INVD.columns.add('quantity', sql.NVarChar(50));
+                INVD.columns.add('value_amount', sql.NVarChar(50));
+                INVD.columns.add('remarks', sql.NVarChar(255));
+
+                customerloadpostmaster.invoiceDetails.forEach(item => {
+                    INVD.rows.add(
+                        item.po_number,
+                        item.invoice_number,
+                        item.invoice_date,
+                        item.quantity,
+                        item.value_amount,
+                        item.remarks
+                    );
+                });
+
+                request.input('invoiceDetails', sql.TVP, INVD);
+
+                request.output('load_master_id', sql.NVarChar(30));
+                request.output('bstatus_code', sql.NVarChar(50));
                 request.output('bmessage_desc', sql.NVarChar(255));
 
-                // Call the stored procedure
                 return request.execute('InsertCustomerLoadPostMaster');
             })
             .then(result => {
-                const output = {
+                resolve({
+                    load_master_id: result.output.load_master_id,
                     bstatus_code: result.output.bstatus_code,
                     bmessage_desc: result.output.bmessage_desc
-                };
-                resolve(output);
+                });
             })
             .catch(err => {
                 reject('SQL Error: ' + err);
@@ -86,7 +105,7 @@ exports.updatecustomerloadpostmasterDB = (customerloadpostmaster) => {
                 const request = pool.request();
 
                 // Input parameters
-                request.input('load_master_id',sql.NVarChar(15), customerloadpostmaster.load_master_id);
+                request.input('load_master_id', sql.NVarChar(15), customerloadpostmaster.load_master_id);
                 request.input('LoadPostID', sql.NVarChar(15), customerloadpostmaster.LoadPostID);
                 request.input('CustomerID', sql.NVarChar(15), customerloadpostmaster.CustomerID);
                 request.input('contact_person', sql.NVarChar(100), customerloadpostmaster.contact_person);
@@ -120,7 +139,7 @@ exports.updatecustomerloadpostmasterDB = (customerloadpostmaster) => {
                 request.input('insurance', sql.Bit, customerloadpostmaster.insurance);
                 request.input('transit_risk', sql.Bit, customerloadpostmaster.transit_risk);
                 request.input('lr_no', sql.NVarChar(50), customerloadpostmaster.lr_no);
-                request.input('lr_date', sql.Date, customerloadpostmaster.lr_date);
+                request.input('lr_date', sql.Date, customerloadpostmaster.lr_date) || null;
                 request.input('po_number', sql.NVarChar(50), customerloadpostmaster.po_number);
                 request.input('invoice_date', sql.NVarChar(50), customerloadpostmaster.invoice_date);
                 // request.input('DATE', sql.Float, customerloadpostmaster.DATE);
@@ -129,7 +148,7 @@ exports.updatecustomerloadpostmasterDB = (customerloadpostmaster) => {
                 request.input('value_amount', sql.NVarChar(200), customerloadpostmaster.value_amount);
                 request.input('remarks', sql.NVarChar(200), customerloadpostmaster.remarks);
 
-                
+
 
                 // Outputs
                 // request.output('load_master_id', sql.NVarChar(255));
@@ -158,12 +177,12 @@ exports.getcustomermasterDB = (data) => {
         sql.connect(pool)
             .then(pool => {
                 const request = pool.request();
-                request.input('CustomerID', sql.NVarChar(10), data.CustomerID );
+                request.input('CustomerID', sql.NVarChar(10), data.CustomerID);
                 request.input('LoadPostID', sql.NVarChar(10), data.LoadPostID);
                 request.input('LoadMasterID', sql.NVarChar(10), data.LoadMasterID);
                 console.log(`[INFO]: Executing Query - SELECT * FROM CustomerLoadPostMaster WHERE LoadPostID = @LoadPostID `);
 
-                if (!data.LoadPostID) { 
+                if (!data.LoadPostID) {
                     return request.query(`SELECT * FROM CustomerLoadPostMaster
                                             WHERE CustomerID = @CustomerID
                                             ORDER BY insert_date DESC;`);
@@ -204,7 +223,7 @@ exports.customerloadpostinvoiceDB = (customerloadpostinvoice) => {
                 request.input('po_number', sql.NVarChar(50), customerloadpostinvoice.po_number);
                 request.input('invoice_date', sql.Date, customerloadpostinvoice.invoice_date);
                 request.input('invoice_number', sql.NVarChar(50), customerloadpostinvoice.invoice_number);
-                request.input('quantity',sql.NVarChar(50), customerloadpostinvoice.quantity);
+                request.input('quantity', sql.NVarChar(50), customerloadpostinvoice.quantity);
                 request.input('value_amount', sql.NVarChar(50), customerloadpostinvoice.value_amount);
                 request.input('remarks', sql.NVarChar(200), customerloadpostinvoice.remarks);
                 // Outputs
@@ -232,7 +251,7 @@ exports.getcustomerloadpostmasterDB = (data) => {
         sql.connect(pool)
             .then(pool => {
                 const request = pool.request();
-                request.input('CustomerID', sql.NVarChar(10), data.CustomerID );
+                request.input('CustomerID', sql.NVarChar(10), data.CustomerID);
                 request.input('LoadPostID', sql.NVarChar(10), data.LoadPostID);
 
                 console.log(`[INFO]: Executing Query - SELECT * FROM CustomerLoadPost WHERE LoadPostID = @LoadPostID `);
