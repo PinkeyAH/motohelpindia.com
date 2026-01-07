b72310cedd5db747a8565a45a8c32662ab1ad641
PinkyV <pinkyahirwar752@gmail.com>
2026-01-07 13:18:45 +0530

    corection

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,5 +1,9 @@
 
-require('dotenv').config({ path: '../.env' });
+//require('dotenv').config({ path: '../.env' });
+require('dotenv').config({
+  path: require('path').resolve(__dirname, '../.env')
+});
+
 const bodyParser = require('body-parser');
 const express = require("express");
 const http = require("http");
