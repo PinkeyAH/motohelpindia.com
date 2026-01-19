263f8b534181928aa6a95ac099cbc2d406bdfb2a
PinkyV <pinkyahirwar752@gmail.com>
2026-01-19 12:42:12 +0530

    corection

--- api-gateway/clearLoads.js
+++ api-gateway/clearLoads.js
@@ -5,6 +5,16 @@ const redis = new Redis({ host: "127.0.0.1", port: 6379 });
 
 async function clearLoads() {
   try {
+
+            // vendor
+    const vendorKeys = await redis.keys("vendor:drivers:*");
+    if (vendorKeys.length > 0) {
+      await redis.del(vendorKeys);
+      console.log("Deleted vendor keys:", vendorKeys);
+    } else {
+      console.log("No vendor keys found.");
+    }
+
         // driver
     const driverKeys = await redis.keys("driver:details:*");
     if (driverKeys.length > 0) {
