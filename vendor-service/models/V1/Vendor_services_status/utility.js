34ff41564f62197ab108678def3101ad29a432fe
PinkyV <pinkyahirwar752@gmail.com>
2026-01-28 14:22:13 +0530

    corevtion

--- vendor-service/models/V1/Vendor_services_status/utility.js
+++ vendor-service/models/V1/Vendor_services_status/utility.js
@@ -21,7 +21,7 @@ exports.GetCustomerLPpendingDB = async (data) => {
         request.input('DriverID', sql.VarChar(50), data.DriverID || null);
         request.input('LP_Status', sql.VarChar(100), data.LPStatus || null);
         request.input('Search', sql.VarChar(100), data.Search || null);
-        request.input('pageNumber', sql.Int, data.pageNumber || 10);
+        request.input('pageNumber', sql.Int, data.pageNumber || 1);
         request.input('pageSize', sql.Int, data.pageSize || 10);
 
         // 📤 OUTPUT PARAMS (from SP)
